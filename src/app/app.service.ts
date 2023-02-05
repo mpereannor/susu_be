8b52b96c8d153032d7217d81152cedb55a82aa49
mpere <mpereannor@gmail.com>
2023-02-05 01:23:06 +0000

    chore: trigger deployment

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '  NOT AGAIN Stop it!';
+    return 'La Meme Gang!';
   }
 }
